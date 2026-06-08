ea09275cdc36eb2d423c6d67fe90e2aa08ba5b0f
BensonZhengFromACCI <343804126@tdsb.ca>
2026-06-08 15:40:47 +0000

    cards

--- Blackjack.java
+++ Blackjack.java
@@ -35,11 +35,11 @@ public class Blackjack extends Application {
         double margin = 10;
 
         /// Card rectangle
-        Rectangle card = new Rectangle(cardWidth, cardHeight);
-        card.setFill(Color.WHITE);
-        card.setStroke(Color.BLACK);
-        card.setArcHeight(10);
-        card.setArcWidth(10);
+        Rectangle cardAce = new Rectangle(cardWidth, cardHeight);
+        cardAce.setFill(Color.WHITE);
+        cardAce.setStroke(Color.BLACK);
+        cardAce.setArcHeight(10);
+        cardAce.setArcWidth(10);
         /// Top A
         Text topLetter = new Text("A");
         topLetter.setFont(Font.font("Arial", FontWeight.BOLD, 24));
@@ -58,17 +58,17 @@ public class Blackjack extends Application {
         bottomLetter.setTranslateY(cardHeight / 2 - margin - 10);
         bottomLetter.setRotate(180);
         /// Group together
-        StackPane aceCard = new StackPane(card, topLetter, bottomLetter);
+        StackPane aceCard = new StackPane(cardAce, topLetter, bottomLetter);
         aceCard.setTranslateX(20);
         aceCard.setTranslateY(-20);
         aceCard.setRotate(10);
 
         /// king card loading screen
-        Rectangle card2 = new Rectangle(cardWidth, cardHeight);
-        card2.setFill(Color.WHITE);
-        card2.setStroke(Color.BLACK);
-        card2.setArcHeight(10);
-        card2.setArcWidth(10);
+        Rectangle cardKing = new Rectangle(cardWidth, cardHeight);
+        cardKing.setFill(Color.WHITE);
+        cardKing.setStroke(Color.BLACK);
+        cardKing.setArcHeight(10);
+        cardKing.setArcWidth(10);
         Text topLetter2 = new Text("K");
         topLetter2.setFont(Font.font("Arial", FontWeight.BOLD, 24));
         topLetter2.setFill(Color.BLACK);
@@ -85,7 +85,7 @@ public class Blackjack extends Application {
         bottomLetter2.setTranslateX(cardWidth / 2 - margin - 10);
         bottomLetter2.setTranslateY(cardHeight / 2 - margin - 10);
 
-        StackPane kingCard = new StackPane(card2, topLetter2, bottomLetter2);
+        StackPane kingCard = new StackPane(cardKing, topLetter2, bottomLetter2);
         kingCard.setTranslateX(-40);
         kingCard.setRotate(-10);
         bottomLetter2.setRotate(180);
@@ -224,9 +224,127 @@ public class Blackjack extends Application {
             Pane gamePane = new Pane(placeYourBet, confirmBtn, times2Btn, 
                                      divide2Btn, displayPotA, displayBal);
             scene.setRoot(gamePane);
+        
+            confirmBtn.setOnMouseClicked( event -> {
+            Pane blackjackPane = new Pane();
+            double buttonWidth = 120;
+            double buttonHeight = 50;
+            double margin2 = 30;
+            double  sceneBottomY = scene.getHeight() - buttonHeight - margin2;
 
+            Deck deck = new Deck();
+
+            String initialCard1 = deck.drawCard();
+            String initialCard2 = deck.drawCard();
+
+            /// draw initial cards with variables
+            /// get the cards and separate value from suit
+            int comma1 = initialCard1.indexOf(",");
+            String value1 = initialCard1.substring(0, comma1);
+            String suit1 = initialCard1.substring(comma1 + 2);
+            int comma2 = initialCard2.indexOf(",");
+            String value2 = initialCard2.substring(0, comma2);
+            String suit2 = initialCard2.substring(comma2 + 2);
+            /// suits to symbols on card visual
+            String suitSymbol1 = "";
+            if (suit1.equals("Hearts")) suitSymbol1 = "♥";
+            else if (suit1.equals("Diamonds")) suitSymbol1 = "♦";
+            else if (suit1.equals("Clubs")) suitSymbol1 = "♣";
+            else if (suit1.equals("Spades")) suitSymbol1 = "♠";
+            String suitSymbol2 = "";
+            if (suit2.equals("Hearts")) suitSymbol2 = "♥";
+            else if (suit2.equals("Diamonds")) suitSymbol2 = "♦";
+            else if (suit2.equals("Clubs")) suitSymbol2 = "♣";
+            else if (suit2.equals("Spades")) suitSymbol2 = "♠";
+            /// first card
+            Rectangle card1 = new Rectangle(100, 140);
+            card1.setFill(Color.WHITE);
+            card1.setStroke(Color.BLACK);
+            card1.setArcWidth(10);
+            card1.setArcHeight(10);
+            Text card1Value = new Text(value1);
+            card1Value.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+            card1Value.setTranslateX(-30);
+            card1Value.setTranslateY(-45);
+            Text card1Suit = new Text(suitSymbol1);
+            card1Suit.setFont(Font.font("Arial", 30));
+            StackPane playerCard1 = new StackPane(card1, card1Value, card1Suit);
+            playerCard1.setLayoutX(250);
+            playerCard1.setLayoutY(150);
+            /// second card
+            Rectangle card2 = new Rectangle(100, 140);
+            card2.setFill(Color.WHITE);
+            card2.setStroke(Color.BLACK);
+            card2.setArcWidth(10);
+            card2.setArcHeight(10);
+            Text card2Value = new Text(value2);
+            card2Value.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+            card2Value.setTranslateX(-30);
+            card2Value.setTranslateY(-45);
+            Text card2Suit = new Text(suitSymbol2);
+            card2Suit.setFont(Font.font("Arial", 30));
+            StackPane playerCard2 = new StackPane(card2, card2Value, card2Suit);
+            playerCard2.setLayoutX(370);
+            playerCard2.setLayoutY(150);
+            /// Stand Button
+            Rectangle standRect = new Rectangle(buttonWidth, buttonHeight);
+            standRect.setArcWidth(20);
+            standRect.setArcHeight(20);
+            standRect.setFill(Color.WHITE);
+            standRect.setStroke(Color.BLACK);
+            standRect.setStrokeWidth(3);
+            Text standTxt = new Text("Stand");
+            standTxt.setFont(Font.font("Times New Roman", FontWeight.BOLD, 18));
+            StackPane standBtn = new StackPane(standRect, standTxt);
+            standBtn.setLayoutX(margin2);
+            standBtn.setLayoutY(sceneBottomY);
+            /// Hit Button
+            Rectangle hitRect = new Rectangle(buttonWidth, buttonHeight);
+            hitRect.setArcWidth(20);
+            hitRect.setArcHeight(20);
+            hitRect.setFill(Color.WHITE);
+            hitRect.setStroke(Color.BLACK);
+            hitRect.setStrokeWidth(3);
+            Text hitTxt = new Text("Hit");
+            hitTxt.setFont(Font.font("Times New Roman", FontWeight.BOLD, 18));
+            StackPane hitBtn = new StackPane(hitRect, hitTxt);
+            hitBtn.setLayoutX(margin2 + (buttonWidth + margin2));
+            hitBtn.setLayoutY(sceneBottomY);
+            /// Split Button
+            Rectangle splitRect = new Rectangle(buttonWidth, buttonHeight);
+            splitRect.setArcWidth(20);
+            splitRect.setArcHeight(20);
+            splitRect.setFill(Color.WHITE);
+            splitRect.setStroke(Color.BLACK);
+            splitRect.setStrokeWidth(3);
+            Text splitTxt = new Text("Split");
+            splitTxt.setFont(Font.font("Times New Roman", FontWeight.BOLD, 18));
+            StackPane splitBtn = new StackPane(splitRect, splitTxt);
+            splitBtn.setLayoutX(margin2 + 2 * (buttonWidth + margin2));
+            splitBtn.setLayoutY(sceneBottomY);
+            /// Double Button
+            Rectangle doubleRect = new Rectangle(buttonWidth, buttonHeight);
+            doubleRect.setArcWidth(20);
+            doubleRect.setArcHeight(20);
+            doubleRect.setFill(Color.WHITE);
+            doubleRect.setStroke(Color.BLACK);
+            doubleRect.setStrokeWidth(3);
+            Text doubleTxt = new Text("Double");
+            doubleTxt.setFont(Font.font("Times New Roman", FontWeight.BOLD, 18));
+            StackPane doubleBtn = new StackPane(doubleRect, doubleTxt);
+            doubleBtn.setLayoutX(margin2 + 3 * (buttonWidth + margin2));
+            doubleBtn.setLayoutY(sceneBottomY);
             
+            blackjackPane.getChildren().addAll(standBtn, hitBtn, card1, 
+                                               card2, splitBtn, doubleBtn);
+            scene.setRoot(blackjackPane);
+            standBtn.setOnMouseClicked(standClicked -> {
+                
+            });
         });
+
+        });
+        
     }
     public class Deck {
         private Queue<String> deck = new LinkedList<>();
@@ -255,7 +373,7 @@ public class Blackjack extends Application {
                 deck.clear();
                 deck.addAll(shuffled);
             }
-            /// draw card
+            /// draw card from deck
             public String drawCard() {
                 if (deck.isEmpty()) {
                     return "Deck is empty";
@@ -268,7 +386,11 @@ public class Blackjack extends Application {
                 /// *(!@U)(*!*(@*!*(!@ make display for this later ********!*!*@!_(@(*@**(!@)!@*)!@*!@)(!@*)!@*!
                 return deck.size();
             }
-        }
+    
+
+    }
+
+
 
     public static void main(String[] args) {
         launch(args);
